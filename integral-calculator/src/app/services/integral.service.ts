4ab85d4190325b31297bbf2be39dc5edee7e1ecb
Karthik <98963132+karthikraghu@users.noreply.github.com>
2025-06-04 04:40:42 +0200

    Update integral.service.ts

--- integral-calculator/src/app/services/integral.service.ts
+++ integral-calculator/src/app/services/integral.service.ts
@@ -19,7 +19,7 @@ export interface IntegralResponse {
   providedIn: 'root'
 })
 export class IntegralService {
-  private apiUrl = 'https://area-py-backend.onrender.com/';
+  private apiUrl = 'https://area-py-backend.onrender.com';
 
   constructor(private http: HttpClient) { }
 
